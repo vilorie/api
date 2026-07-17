a7a9bba635e18436733441aaff25aad373f8c201
RinnAnd <andre.ksas@gmail.com>
2026-07-17 16:44:42 -0500

    feat: customer type

---
+++ models/models.ts
@@ -0,0 +1,9 @@
+export type Customer = {
+  company_name: string;
+  engineer: string;
+  engineer_mail: string;
+  birthday: string;
+  frequency: string;
+  recolection_date: string;
+  load_avg: number;
+}
